f8686b6987066875e557f63dbf34a9f7a23a2646
Neill Jordan <neilljordan@gmail.com>
2013-01-19 15:02:53 -0500

    now uses WebSocket4Net. added nuspec.

---
+++ PusherClientDotNet/WebSocket4NetProxy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocket4Net;
+
+namespace PusherClientDotNet
+{
+    class WebSocket4NetProxy : WebSocket4Net.WebSocket, IWebSocket
+    {
+        public WebSocket4NetProxy(string url)
+            : base(url)
+        {
+            Opened += (o, e) => { if (OnOpen != null) OnOpen(this, e); };
+            Closed += (o, e) => { if (OnClose != null) OnClose(this, e); };
+            MessageReceived += (o, e) => { if (OnData != null) OnData(this, e); };            
+        }
+
+        public event EventHandler<EventArgs> OnClose;
+
+        public event EventHandler<MessageReceivedEventArgs> OnData;
+
+        public event EventHandler<EventArgs> OnOpen;
+
+        public void SendMessage(string data)
+        {
+            Send(data);
+        }
+    }
+}
